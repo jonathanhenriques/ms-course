b34f41c158adc2e0ca02238288329cec339fa0a5
Jonathan Henrique <silva.henriquejonas@gmail.com>
2024-07-03 17:41:19 -0300

    hr-payroll implementado restTemplate para hr-worker

---
+++ hr-payroll/src/main/java/com/jonathan/hrpayroll/resources/PaymentResource.java
@@ -0,0 +1,26 @@
+package com.jonathan.hrpayroll.resources;
+
+import com.jonathan.hrpayroll.entities.Payment;
+import com.jonathan.hrpayroll.services.PaymentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/payments")
+public class PaymentResource {
+
+    private PaymentService service;
+
+    public PaymentResource(PaymentService service) {
+        this.service = service;
+    }
+
+    @GetMapping(value = "/{workerId}/days/{days}")
+    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
+        Payment payment = service.getPayment(workerId, days);
+        return  ResponseEntity.ok(payment);
+    }
+}
---
+++ hr-payroll/src/main/java/com/jonathan/hrpayroll/services/PaymentService.java
@@ -0,0 +1,32 @@
+package com.jonathan.hrpayroll.services;
+
+import com.jonathan.hrpayroll.entities.Payment;
+
+import com.jonathan.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class PaymentService {
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    private RestTemplate restTemplate;
+
+    public PaymentService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public Payment getPayment(long workerId, int days){
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", ""+workerId);
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
+    }
+}
